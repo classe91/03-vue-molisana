91aa3aa6078780e6a3904d5c92bd55631b7c09d2
Donato Riccio <donniepask@gmail.com>
2023-04-13 17:33:59 +0200

    Add files via upload

---
+++ src/components/SiteMain.vue
@@ -0,0 +1,18 @@
+<script>
+import ProductsList from './ProductsList.vue'
+export default {
+  name: 'SiteMain',
+  components: {
+    ProductsList
+  }
+}
+</script>
+<template>
+  <main id="site-main">
+    <ProductsList />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
